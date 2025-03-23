486508a575912717b25463f151df6e8975ae671e
Dominik Prochazka <domm98cz@gmail.com>
2025-03-23 17:34:39 +0100

    fix init scripts and add debug mode for dummy data

--- resources/scripts/install.bat
+++ resources/scripts/install.bat
@@ -2,4 +2,6 @@
 cd ..\..
 docker network create backend_with_database
 docker-compose up -d
+docker exec -it backend bash -c "composer install"
+docker exec -it backend bash -c "php bin/console migrations:reset"
 echo Application starts on http://localhost:8080
